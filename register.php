69654e5b78b187bc5c4b17726121a9527b027db9
Jonathan Frank Steele <jonathan@steeleconsult.com>
2017-10-31 21:50:44 -0600

    Got Register up and running

--- register.php
+++ register.php
@@ -1,87 +1,112 @@
 <?php
   session_start();
   require 'MySqlInfo.php';
+  //Declaration of Variables for Register.
   $LoginID = "";
-  $loginpassword = "";
+  $LoginIDErr = "";
+  $Email = "";
+  $EmailErr = "";
+  $Name = "";
+  $NameErr = "";
+  $LoginPassword = "";
+  $LoginPasswordErr = "";
+  $VerifyLoginPassword = "";
+  $VerifyLoginPasswordErr = "";
+  $CheckLoginID = "";
+  $CheckEmail = "";
   if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    function test_input($data) {
+    function Input($data) {
       $data = trim($data);
       $data = stripslashes($data);
       $data = htmlspecialchars($data);
       return $data;
     }
-
-    if (empty($_POST["loginid"])) {
-      $emailErr = "loginID is required";
+    // Check if LoginID is empty
+    if (empty($_POST["LoginID"])) {
+      $LoginIDErr = "LoginID is required";
     } else {
-      $LoginID = test_input($_POST["loginid"]);
+      $LoginID = Input($_POST["LoginID"]);
+      // echo $LoginID."<br />";
       // check if name only contains letters and whitespace
       if (!preg_match("/^[a-zA-Z ]*$/",$LoginID)) {
-        $emailErr = "Only letters and white space allowed";
+        $LoginIDErr = "Only letters and white space allowed";
       }
+    }
+    // Check if Email is empty
+    if (empty($_POST["Email"])) {
+      $EmailErr = "Email is required";
+    } else {
+      $Email = Input($_POST["Email"]);
+      // echo $Email."<br />";
+      // check if name only contains letters and whitespace
+      // if (!preg_match("/^[a-zA-Z ]*$/",$Email)) {
+      //   $EmailErr = "Only letters and white space allowed";
+      // }
+    }
+    // Check if Name is empty
+    if (empty($_POST["Name"])) {
+      $NameErr = "Name is required";
+    } else {
+      $Name = Input($_POST["Name"]);
+      // echo $Name."<br />";
+      // check if name only contains letters and whitespace
+      if (!preg_match("/^[a-zA-Z ]*$/",$Name)) {
+        $NameErr = "Only letters and white space allowed";
+      }
+    }
+    // Check if LoginPassword is empty
+    if (empty($_POST["LoginPassword"])) {
+      $LoginPasswordErr = "Password is required";
+    } else {
+      $LoginPassword = Input($_POST["LoginPassword"]);
+      // echo $LoginPassword."<br />";
+      // check if name only contains letters and whitespace
+      // if (!preg_match("/^[a-zA-Z ]*$/",$LoginPassword)) {
+      //   $LoginPasswordErr = "Only letters and white space allowed";
+      // }
+    }
+    // Check if VerifyLoginPassword is empty
+    if (empty($_POST["VerifyLoginPassword"])) {
+      $VerifyLoginPasswordErr = "Verified Login Password is required";
+    } else {
+      $VerifyLoginPassword = Input($_POST["VerifyLoginPassword"]);
+      // echo $VerifyLoginPassword."<br />";
+      // check if name only contains letters and whitespace
+      // if (!preg_match("/^[a-zA-Z ]*$/",$VerifyLoginPassword)) {
+      //   $VerifyLoginPasswordErr = "Only letters and white space allowed";
+      // }
     }
 
-    if (empty($_POST["name"])) {
-      $loginpasswordErr = "Name is required";
-    } else {
-      $name = test_input($_POST["name"]);
-      // check if name only contains letters and whitespace
-      if (!preg_match("/^[a-zA-Z ]*$/",$loginpassword)) {
-        $passwordErr = "Only letters and white space allowed";
-      }
-    }
-
-    if (empty($_POST["loginpassword"])) {
-      $loginpasswordErr = "Password is required";
-    } else {
-      $loginpassword = test_input($_POST["loginpassword"]);
-      // check if name only contains letters and whitespace
-      if (!preg_match("/^[a-zA-Z ]*$/",$loginpassword)) {
-        $passwordErr = "Only letters and white space allowed";
-      }
-    }
-
-    if (empty($_POST["loginpassword"])) {
-      $loginpasswordErr = "Password is required";
-    } else {
-      $loginpassword = test_input($_POST["loginpassword"]);
-      // check if name only contains letters and whitespace
-      if (!preg_match("/^[a-zA-Z ]*$/",$loginpassword)) {
-        $passwordErr = "Only letters and white space allowed";
-      }
-    }
   try {
+    if($LoginPassword == $VerifyLoginPassword)
+    {
       $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
       $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-      $stmt = $conn->prepare("INSERT INTO Player(LoginID, Name, Email, Password) VALUES(:loginID, :loginEmail, :loginName, :loginPassword)");
-      $stmt->bindParam(':loginID', $LoginID);
-      $stmt->bindParam(':loginEmail', $email);
-      $stmt->bindParam(':loginName', $name);
-      $stmt->bindParam(':loginPassword', $loginpassword);
-      $stmt->execute();
-      // $RegisterSuccessTF = true;
-
-      // set the resulting array to associative
-      // $result = $stmt->setFetchMode(PDO::FETCH_ASSOC);
-      // foreach (new RecursiveArrayIterator($stmt->fetchAll()) as $k=>$v) {
-      //     // echo $v;
-      //     $LoginSuccessTF = false;
-      // }
-      //Successfull Login if LoginSuccessTF = True
-      // if($LoginSuccessTF)
-      // {
-        // Set session variables
-        // $_SESSION["LoginID"] = $LoginID;
-        // $_SESSION["Role"]= "Player"; //This should come from the database
-        //
-        // if (headers_sent()) {
-        //   die("Redirect failed. Please click on this link: <a href=index.php>Index</a>");
-        // }
-        // else{
-        //   header("Location: index.php");
-        // }
-      // }
-
+      $ExistCheckStmt = $conn->prepare("SELECT LoginID, Email FROM Player WHERE LoginID = :RegisterID OR Email = :RegisterEmail");
+      $ExistCheckStmt->bindParam(':RegisterID', $LoginID);
+      $ExistCheckStmt->bindParam(':RegisterEmail', $Email);
+      $ExistCheckStmt->execute();
+      $count = $ExistCheckStmt->rowCount();
+      //print_r($count);
+      if($count == 0){
+        $LoginIDErr = "";
+        $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        $stmt = $conn->prepare("INSERT INTO Player(LoginID, Email, Name, Password) VALUES(:RegisterID, :RegisterEmail, :RegisterName, :RegisterPassword)");
+        $stmt->bindParam(':RegisterID', $LoginID);
+        $stmt->bindParam(':RegisterEmail', $Email);
+        $stmt->bindParam(':RegisterName', $Name);
+        $stmt->bindParam(':RegisterPassword', $LoginPassword);
+        $stmt->execute();
+        header("Location: login.php?msg=New+User+Created+Successfully.");
+      }
+      else {
+        $LoginIDErr = "User Already Exists";
+      }
+    }
+    else {
+      $VerifyLoginPasswordErr = "Passwords dont match..";
+    }
   } catch (PDOException $e) {
       echo "Error: " . $e->getMessage();
   }
@@ -103,19 +128,24 @@
       <h2 style="text-align: center;">Register</h2>
       <br />
       <label class="sr-only">Login ID</label>
-      <input class="form-control" name="loginid" placeholder="Login ID" value="<?php echo $LoginID ?>" required autofocus autocomplete="off" />
+      <input class="form-control" name="LoginID" placeholder="Login ID" value="<?php echo $LoginID ?>" required autofocus autocomplete="off" />
+      <span style="color:red;"><?php echo $LoginIDErr ?></span>
       <br />
       <label class="sr-only">Email</label>
-      <input class="form-control" name="email" placeholder="Email" value="<?php echo $email ?>" required autofocus autocomplete="off" />
+      <input class="form-control" name="Email" placeholder="Email" value="<?php echo $Email ?>" required autofocus autocomplete="off" />
+      <span style="color:red;"><?php echo $EmailErr ?></span>
       <br />
       <label class="sr-only">Name</label>
-      <input class="form-control" name="name" placeholder="Name" value="<?php echo $name ?>" required autofocus autocomplete="off" />
+      <input class="form-control" name="Name" placeholder="Name" value="<?php echo $Name ?>" required autofocus autocomplete="off" />
+      <span style="color:red;"><?php echo $NameErr ?></span>
       <br />
       <label class="sr-only">Password</label>
-      <input class="form-control" name="loginpassword" placeholder="Password" value="<?php echo $loginpassword ?>" required autofocus autocomplete="off" />
+      <input class="form-control" name="LoginPassword" type="password" placeholder="Password" value="<?php echo $LoginPassword ?>" required autofocus autocomplete="off" />
+      <span style="color:red;"><?php echo $LoginPasswordErr ?></span>
       <br />
       <label class="sr-only">Verify Password</label>
-      <input class="form-control" name="loginPasswordVerify" placeholder="Verify Password" value="<?php echo $loginPasswordVerify ?>" required autofocus autocomplete="off" />
+      <input class="form-control" name="VerifyLoginPassword" type="password" placeholder="Verify Password" value="<?php echo $VerifyLoginPassword ?>" required autofocus autocomplete="off" />
+      <span style="color:red;"><?php echo $VerifyLoginPasswordErr ?></span>
       <br />
       <button class="btn btn-lg btn-primary btn-block" type="submit" name="submit" value="Submit">Register</button>
       <br />
